d4c74390e877f69a49a9f5b301fcef81a3846ea7
Rocio-anabel <163307652+Rocio-anabel@users.noreply.github.com>
2026-05-21 20:09:39 -0300

    Feat: Añadido el modelo Usuario

---
+++ models/Usuario.js
@@ -0,0 +1,57 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from '../config/database.js';
+
+export class Usuario extends Model {}
+
+Usuario.init(
+    {
+        idUsuario: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        apellido: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        sexo:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        fechaNacimiento: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        contraseña: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        bio: {
+            type: DataTypes.TEXT,
+        },
+        avatar: {
+            type: DataTypes.BLOB,
+        },
+        esModerador: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+    },
+    {
+        sequelize,
+        underscored: true,
+        timestamps: true,
+        createdAt: 'fecha_creacion',
+        updatedAt: 'fecha_actualizacion',
+        paranoid: true,
+        deletedAt: 'fecha_borrado'
+    }
+)
